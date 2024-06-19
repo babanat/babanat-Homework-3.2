9d81d49dc9559d0944ed480ab7af0944c24b93de
babanat <babanat3@gmail.com>
2024-06-19 08:57:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+ //    "String"
+ const greeting = "Welkome";
+ const isName = "Kosta";
+ const isSoname = "Molodanov";
+ const message = `${greeting}, ${isName} ${isSoname}!`;
+//  console.log(message)
